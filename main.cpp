e12f87ed21c27a7080cbd9c90409e75332be27ae
Alexander Platonov <144921734+sashokdimasik@users.noreply.github.com>
2023-09-28 10:01:03 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,71 @@
+/**********************************************
+ * Author:       Platonov A.A.                *
+ * Group:        ПИ-231                       *
+ * Variant:      14                           *
+ * Date:         27.09.2023                   *
+ * Project name: Huge Filthy Pile of Printers *
+ * OnlineGDB: https://onlinegdb.com/1Q9OMD_-7 *
+ **********************************************/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string fname, typeToShow, brandCheap, brandExpensive, numberCheap, numberExpensive;
+  int costMin, costMax = -1, printersCount = 0;
+  double costAverage = 0;
+  
+  cout << "Enter file name: ";
+  cin >> fname; // Enter input.txt if you checking it on OnlineGDB
+  
+  ifstream fin(fname);
+  
+  if (!fin) {
+    cout << "\nACHTUNG! Couldn't read the file 'input.txt'!";
+    return 1;
+  }
+  
+  cout << "Enter type of printers to show (Laser or DotMatrix): ";
+  cin >> typeToShow;
+  
+  while (!fin.eof()) {
+    string brand, type, number;
+    int cost;
+    
+    fin >> brand >> type >> number >> cost;
+    
+    if (type == typeToShow) {
+      if (printersCount == 0) costMin = cost;
+      
+      printersCount++;
+      costAverage += cost;
+      
+      cout << brand << ' ' << number << ", cost: " << cost << '\n';
+      
+      if (cost > costMax) {
+        costMax = cost;
+        numberExpensive = number;
+        brandExpensive = brand;
+      }
+      
+      if (cost < costMin) {
+        costMin = cost;
+        numberCheap = number;
+        brandCheap = brand;
+      }
+    }
+  }
+  
+  fin.close();
+  
+  costAverage /= printersCount;
+  
+  cout << "\nAverage cost of " << typeToShow << " printers: " << costAverage
+       << "\nThe most cheap printer: " << brandCheap << ' ' << numberCheap
+       << "\nThe most expensive printer: " << brandExpensive << ' ' << numberExpensive;
+  
+  return 0;
+}
